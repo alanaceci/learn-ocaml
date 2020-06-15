1ff6581643058f8ce7b3131bd10c0998bd56f058
Alana Ceci <alanaceci@MacBook-Pro.local>
2020-06-15 16:23:57 -0600

    Revert "Revert "feat: environment variables, merged versions of LOC""
    This reverts commit 506b7110ecf27690309416eed50a108a26fa4b00.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8000;
+const database = process.env.DB_CONN;
+const port = process.env.PORT;
+const request = process.env.REQ;
+const collection = process.env.COLLECTION;
+// const port = 8000;
 const mongoose = require('mongoose');
+
 app.use(bodyParser.text({ type: "application/json" }));
 
 // connect to database
-mongoose.connect('mongodb://172.17.0.1/learn-ocaml-code');
+// mongoose.connect('mongodb://172.17.0.1/learn-ocaml-code');
+mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true} );
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -17,13 +23,14 @@ app.use(function(req, res, next) {
     next();
   });
 
+
 // receive the POST from the client javascript file
 app.post("/", function (req, res)
 {
   if (req.body)
   {
     const solution = JSON.parse( req.body ); // parse req.body as an object
-    db.collection('studentcode').insertOne(solution);
+    db.collection(collection).insertOne(solution);
     console.log(solution);
     res.sendStatus(200); // success status
   }
