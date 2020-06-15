506b7110ecf27690309416eed50a108a26fa4b00
Alana Ceci <alanaceci@MacBook-Pro.local>
2020-06-15 15:18:44 -0600

    Revert "feat: environment variables, merged versions of LOC"
    This reverts commit 9bd73d8f221f89047a6a7a13f55f7d3418dd7dbf.

--- static/js/get-local-changes.js
+++ static/js/get-local-changes.js
@@ -1,30 +1,24 @@
 track_local_changes();
 
 function track_local_changes() {
-    const request = new XMLHttpRequest();
-    const path = "//localhost:8000"; // server ip and port number
-    try{
-        request.open("POST", path, true); // true = asynchronous
-        request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-        // innerHTML contains nested HTML tags which encapsulate the code 
-        const exercise = document.getElementsByClassName("ace_layer ace_text-layer")[0].innerHTML
-        // strip of HTML to get raw code 
-        const stringSolution = strip(exercise)
-        // get student nickname 
-        const nickname = document.getElementById("learnocaml-nickname").innerHTML
-        // create Object 
-        const obj = new Object();
-        obj.nickname = nickname;
-        obj.timestamp = Date.now();
-        obj.solution  = stringSolution;
-        const jsonString= JSON.stringify(obj);
-        // send to Database
-        request.send (jsonString);
-    }
-    catch (e) {
-        console.log(e);
-    }
-  
+    var request = new XMLHttpRequest();
+    var path = "https://172.17.0.1:8000"; // server ip and port number
+    request.open("POST", path, true); // true = asynchronous
+    request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    // innerHTML contains nested HTML tags which encapsulate the code 
+    const exercise = document.getElementsByClassName("ace_layer ace_text-layer")[0].innerHTML
+    // strip of HTML to get raw code 
+    const stringSolution = strip(exercise)
+    // get student nickname 
+    const nickname = document.getElementById("learnocaml-nickname").innerHTML
+    // create Object 
+    const obj = new Object();
+    obj.nickname = nickname;
+    obj.timestamp = Date.now();
+    obj.solution  = stringSolution;
+    const jsonString= JSON.stringify(obj);
+    // send to Database
+    request.send (jsonString);
 }
 
 function strip(html){
@@ -32,5 +26,9 @@ function strip(html){
     tmp.innerHTML= html
     return tmp.textContent || tmp.innerText || "" 
 }
+
+
+
+
 
 
